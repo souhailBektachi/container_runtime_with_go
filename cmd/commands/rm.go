8192342062003f828f9b0c40c8b8bac727f6e97b
Souhail Bektachi <souhalshiv@gmail.com>
2025-04-19 21:19:42 +0100

    refactor rm command and deleteContainer function: enhance error handling and improve user feedback during container removal

--- cmd/commands/rm.go
+++ cmd/commands/rm.go
@@ -1,1 +1,27 @@
 package commands
+
+import (
+	"fmt"
+
+	"github.com/souhailBektachi/container_runtime_with_go/pkg/run"
+	"github.com/spf13/cobra"
+)
+
+var rmCmd = &cobra.Command{
+	Use:   "rm [containerID...]",
+	Short: "Remove one or more containers",
+	Args:  cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var finalErr error
+		for _, containerID := range args {
+			fmt.Printf("Attempting to remove container %s...\n", containerID)
+			if err := run.DeleteContainer(containerID); err != nil {
+				fmt.Printf("Error removing container %s: %v\n", containerID, err)
+				if finalErr == nil {
+					finalErr = fmt.Errorf("failed to remove container(s)")
+				}
+			}
+		}
+		return finalErr
+	},
+}
--- pkg/run/delete.go
+++ pkg/run/delete.go
@@ -1,1 +1,25 @@
 package run
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func DeleteContainer(containerID string) error {
+	containerBasePath := filepath.Join("_containers", containerID)
+
+	if _, err := os.Stat(containerBasePath); os.IsNotExist(err) {
+		return fmt.Errorf("container '%s' not found", containerID)
+	} else if err != nil {
+		return fmt.Errorf("failed to stat container directory '%s': %w", containerBasePath, err)
+	}
+
+	fmt.Printf("Removing container directory: %s\n", containerBasePath)
+	if err := os.RemoveAll(containerBasePath); err != nil {
+		return fmt.Errorf("failed to remove container directory '%s': %w", containerBasePath, err)
+	}
+
+	fmt.Printf("Successfully removed container '%s'\n", containerID)
+	return nil
+}
