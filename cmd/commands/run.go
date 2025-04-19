01db05a30098d2e3d872baf5483d4cfde64cfc47
Souhail Bektachi <souhalshiv@gmail.com>
2025-04-19 18:40:31 +0100

    refactor run package: implement pivotRoot and applyNamespaces functions with improved error handling

--- cmd/commands/run.go
+++ cmd/commands/run.go
@@ -1,27 +1,7 @@
 package commands
 
-import (
-	"fmt"
-	"os"
+import "strings"
 
-	"github.com/souhailBektachi/container_runtime_with_go/pkg/run"
-)
+func doesContainerExist(name, containerdir string) bool {
 
-func RunCommand() error {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: mini-container run <command>")
-		os.Exit(1)
-	}
-
-	command := os.Args[2]
-	args := []string{}
-	if len(os.Args) > 3 {
-		args = os.Args[3:]
-	}
-
-	if err := run.RunContainer(command, args); err != nil {
-		return fmt.Errorf("error running container: %w", err)
-	}
-
-	return nil
 }
--- pkg/run/run.go
+++ pkg/run/run.go
@@ -4,42 +4,69 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"syscall"
 )
-
-func RunContainer(command string, args []string) error {
-	// Look up the absolute path of the command
-	cmdPath, err := exec.LookPath(command)
-	if err != nil {
-		return fmt.Errorf("command not found: %v", err)
-	}
-
-	cmd := exec.Command(cmdPath, args...)
-	applyNamespaces(cmd)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Start(); err != nil {
-		return fmt.Errorf("failed to start container: %v", err)
-	}
-
-	if err := cmd.Wait(); err != nil {
-		return fmt.Errorf("container exited with error: %v", err)
-	}
-
-	return nil
-}
 
 func applyNamespaces(cmd *exec.Cmd) {
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWNS |
-			syscall.CLONE_NEWIPC |
-			syscall.CLONE_NEWNET |
-			syscall.CLONE_NEWUSER |
+		Cloneflags: syscall.CLONE_NEWUTS |
 			syscall.CLONE_NEWPID |
-			syscall.CLONE_NEWUTS,
+			syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWIPC |
+			syscall.CLONE_NEWUSER |
+			syscall.CLONE_NEWNET,
+		UidMappings: []syscall.SysProcIDMap{{ContainerID: 0, HostID: os.Getuid(), Size: 1}},
+		GidMappings: []syscall.SysProcIDMap{{ContainerID: 0, HostID: os.Getgid(), Size: 1}},
 	}
 
+}
+
+func pivotRoot(newroot string) error {
+	newrootAbs, err := filepath.Abs(newroot)
+	if err != nil {
+		return fmt.Errorf("failed to get absolute path for newroot '%s': %w", newroot, err)
+	}
+
+	if err := syscall.Mount(newrootAbs, newrootAbs, "", syscall.MS_BIND|syscall.MS_REC|syscall.MS_PRIVATE, ""); err != nil {
+		return fmt.Errorf("failed to bind mount newroot '%s' onto itself: %w", newrootAbs, err)
+	}
+
+	putold := filepath.Join(newrootAbs, ".pivot_root")
+	if err := os.MkdirAll(putold, 0700); err != nil {
+		syscall.Unmount(newrootAbs, syscall.MNT_DETACH)
+		return fmt.Errorf("failed to create putold directory '%s': %w", putold, err)
+	}
+
+	if err := syscall.PivotRoot(newrootAbs, putold); err != nil {
+		os.RemoveAll(putold)
+		syscall.Unmount(newrootAbs, syscall.MNT_DETACH)
+		return fmt.Errorf("pivot_root('%s', '%s') failed: %w", newrootAbs, putold, err)
+	}
+
+	if err := syscall.Chdir("/"); err != nil {
+		return fmt.Errorf("chdir to new root '/' failed after pivot_root: %w", err)
+	}
+
+	putoldRelative := "/.pivot_root"
+	if err := syscall.Unmount(putoldRelative, syscall.MNT_DETACH); err != nil {
+		fmt.Fprintf(os.Stderr, "warning: unmount old root '%s' failed: %v\n", putoldRelative, err)
+	}
+
+	if err := os.RemoveAll(putoldRelative); err != nil {
+		fmt.Fprintf(os.Stderr, "warning: failed to remove '%s': %v\n", putoldRelative, err)
+	}
+
+	return nil
+}
+func applyChroot(imageconf ImageConfig) error {
+	if err := pivotRoot(imageconf.Rootfs); err != nil {
+		return fmt.Errorf("Error applying pivot_root: %v", err)
+	}
+
+	if err := syscall.Chdir(imageconf.ProcessConfig.Cwd); err != nil {
+		return fmt.Errorf("Error changing directory: %v", err)
+	}
+	return nil
 }
